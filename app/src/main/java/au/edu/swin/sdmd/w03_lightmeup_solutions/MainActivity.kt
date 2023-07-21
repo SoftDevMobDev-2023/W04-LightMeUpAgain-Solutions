68874db89f102fd14934f604a1e11681b5e137a4
N. A. Ronald <nronald@swin.edu.au>
2023-07-21 15:22:52 +1000

    Added third image and view model.

--- app/src/main/java/au/edu/swin/sdmd/w03_lightmeup_solutions/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w03_lightmeup_solutions/MainActivity.kt
@@ -3,25 +3,40 @@ package au.edu.swin.sdmd.w03_lightmeup_solutions
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.activity.viewModels
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.lifecycle.ViewModel
 
 class MainActivity : AppCompatActivity() {
-    var state = R.drawable.ic_assignment_turned_in_24px
+    val imageViewModel: ImageViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageDrawable(AppCompatResources.getDrawable(this, imageViewModel.state))
+
         imageView.setOnClickListener {
             // update the image state
-            state = when (state) {
-                R.drawable.ic_assignment_turned_in_24px -> R.drawable.ic_assignment_late_24px
-                R.drawable.ic_assignment_late_24px -> R.drawable.ic_assignment_turned_in_24px
-                else -> R.drawable.ic_assignment_turned_in_24px
-            }
+            imageViewModel.nextImage()
             // then show it on screen
-            imageView.setImageDrawable(AppCompatResources.getDrawable(this, state))
+            imageView.setImageDrawable(AppCompatResources.getDrawable(this, imageViewModel.state))
         }
     }
+}
+
+class ImageViewModel: ViewModel() {
+    // this keeps track of the current image
+    var state = R.drawable.ic_assignment_turned_in_24px
+
+    fun nextImage() {
+        state = when (state) {
+            R.drawable.ic_assignment_turned_in_24px -> R.drawable.ic_assignment_late_24px
+            R.drawable.ic_assignment_late_24px -> R.drawable.ic_assignment_returned_black_24dp
+            R.drawable.ic_assignment_returned_black_24dp -> R.drawable.ic_assignment_turned_in_24px
+            else -> R.drawable.ic_assignment_turned_in_24px
+        }
+    }
+
 }
